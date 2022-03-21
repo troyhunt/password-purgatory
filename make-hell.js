99e57fc101c464bcc7314ea29484f3752106d5b1
Troy Hunt <github@troyhunt.com>
2022-03-22 09:04:09 +1000

    ToDo has been done

--- make-hell.js
+++ make-hell.js
@@ -1,9 +1,3 @@
-/*
-ToDo: async request to https://api.passwordpurgatory.com/make-hell?password=foo on form submit
-This script should be embedable in an external page on another site (such as troyhunt.com!) so it's
-an easy drop-in solution.
-*/
-
 function submit(password) {
     var urlEncodedPassword = encodeURIComponent(password)
     var xhr = new XMLHttpRequest();
