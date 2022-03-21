a1a4884ee4645dce0a393d1a8f79135310435187
Troy Hunt <github@troyhunt.com>
2022-03-22 09:59:57 +1000

    Very rough but functional implementation

--- make-hell.js
+++ make-hell.js
@@ -3,10 +3,11 @@ function submit(password) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status == 200) {
-            alert(xhr.response);
+            var json = JSON.parse(xhr.response);
+            document.getElementById('response').innerHTML=json.message;
         }
     }
-    xhr.open('post', 'https://api.passwordpurgatory.com/make-hell?password=${urlEncodedPassword}', true);
+    xhr.open('post', 'https://api.passwordpurgatory.com/make-hell?password=' + urlEncodedPassword, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     xhr.send();
 }
