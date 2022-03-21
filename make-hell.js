d12d5892a29f8bd5d5ed28338755e58a37310e0e
Luke Faccin <79387068+lukefaccin@users.noreply.github.com>
2022-03-21 22:08:37 +1100

    Add basic POST functionality

--- make-hell.js
+++ make-hell.js
@@ -3,3 +3,16 @@ ToDo: async request to https://api.passwordpurgatory.com/make-hell?password=foo 
 This script should be embedable in an external page on another site (such as troyhunt.com!) so it's
 an easy drop-in solution.
 */
+
+function submit(password) {
+    var urlEncodedPassword = encodeURIComponent(password)
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status == 200) {
+            alert(xhr.response);
+        }
+    }
+    xhr.open('post', `https://api.passwordpurgatory.com/make-hell?password=${urlEncodedPassword}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.send();
+}
