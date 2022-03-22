96a1631a42572b9a194e3a948cb500f912463b29
Chrispy <cpetta@oaisd.org>
2022-03-22 12:26:19 -0400

    Refactor submitHell to use a fetch request

--- make-hell.js
+++ make-hell.js
@@ -1,13 +1,14 @@
-function submitHell(password) {
-    var urlEncodedPassword = encodeURIComponent(password)
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status == 200) {
-            var json = JSON.parse(xhr.response);
-            document.getElementById('response').innerHTML=json.message;
-        }
-    }
-    xhr.open('post', 'https://api.passwordpurgatory.com/make-hell?password=' + urlEncodedPassword, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    xhr.send();
+async function submitHell(password, feedback) {
+    let url = `https://api.passwordpurgatory.com/make-hell?password=${encodeURIComponent(password)}`;
+    let headers = new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    });
+    let params = {
+        method: "post",
+        headers,
+        mode: 'cors'
+    };
+    let response = await fetch(url, params);
+    let json = await response.json();
+    feedback.innerHTML = json.message;
 }
