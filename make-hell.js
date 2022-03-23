8a043ac08a8c5f6fda494a552ba5f9a1fdfcd83b
Troy Hunt <github@troyhunt.com>
2022-03-23 10:53:23 +1000

    Code formatting using Prettier

--- make-hell.js
+++ make-hell.js
@@ -1,13 +1,18 @@
 async function submitHell(password, feedback) {
-    let url = `https://api.passwordpurgatory.com/make-hell?password=${encodeURIComponent(password)}`;
-    let headers = new Headers({
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    });
-    let params = {
-        headers,
-        mode: 'cors'
-    };
-    let response = await fetch(url, params);
-    let json = await response.json();
-    feedback.innerHTML = json.message;
+  let url = `https://api.passwordpurgatory.com/make-hell?password=${encodeURIComponent(
+    password
+  )}`;
+
+  let headers = new Headers({
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+  });
+
+  let params = {
+    headers,
+    mode: "cors",
+  };
+
+  let response = await fetch(url, params);
+  let json = await response.json();
+  feedback.innerHTML = json.message;
 }
